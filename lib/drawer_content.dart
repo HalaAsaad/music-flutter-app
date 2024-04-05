1cbc11741406598bdeb867aa578725b5f605b96f
wasim alwisi <wasimalwisi@gmail.com>
2024-04-05 17:56:32 +0300

    darwer edit

--- lib/drawer_content.dart
+++ lib/drawer_content.dart
@@ -54,7 +54,7 @@ class MyDrawerContent extends StatelessWidget {
             },
           ),
           ListTile(
-            title: const Text('Author Searchs'),
+            title: const Text('Author Search'),
             onTap: () {
               // Update the state of the app.
               Navigator.pop(context);
